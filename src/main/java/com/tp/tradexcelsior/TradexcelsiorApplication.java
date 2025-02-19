fc56ac11d370e351ce9551269c18e94cb42f7756
Shreya S <ssshreya2000@gmail.com>
2025-02-19 11:24:27 +0530

    Initial commit

---
+++ src/main/java/com/tp/tradexcelsior/TradexcelsiorApplication.java
@@ -0,0 +1,13 @@
+package com.tp.tradexcelsior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradexcelsiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradexcelsiorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp/tradexcelsior/TradexcelsiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.tradexcelsior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradexcelsiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
