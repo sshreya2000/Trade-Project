fa3872cf9c325f5c1e5a2c636bd5b3a165451ad2
Shreya S <ssshreya2000@gmail.com>
2025-03-07 13:22:36 +0530

    updated mongo

--- src/main/java/com/tp/tradexcelsior/TradexcelsiorApplication.java
+++ src/main/java/com/tp/tradexcelsior/TradexcelsiorApplication.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @OpenAPIDefinition(
@@ -15,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 )
 @SpringBootApplication
 @EnableScheduling
+@EnableMongoRepositories
 public class TradexcelsiorApplication {
 
 	public static void main(String[] args) {
